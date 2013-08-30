15578d2ef226b45336a5c06d94ddb1480e53d338
Ole Michaelis <Ole.Michaelis@googlemail.com>
2013-08-30 16:55:04 +0200

    pushing and fetching phrases working

--- web.rb
+++ web.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'redis'
+
+configure do
+	uri = URI.parse(ENV["REDISTOGO_URL"])
+	set :redis, Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
 
 get '/' do
-    "Hello, world"
+	settings.redis.spop :phrases
+end
+
+post '/' do
+	request.body.rewind  # in case someone already read it
+	settings.redis.sadd :phrases, request.body.read
 end
